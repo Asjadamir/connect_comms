0a17e607e62f7f798ba6b0f00d51c7e7ea077d6f
Asjad Amir <asjadamir112@gmail.com>
2024-08-13 20:45:46 +0500

    Fix UI Bugs

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { BeatLoader } from "react-spinners";
 
 const Loader = () => {
     return (
-        <div className="w-full fixed flex items-center justify-center top-0 right-0 h-full bg-neutral-50 z-50">
+        <div className="w-screen fixed flex items-center justify-center top-0 left-0 h-screen bg-neutral-50 z-50">
             <BeatLoader />
         </div>
     );
